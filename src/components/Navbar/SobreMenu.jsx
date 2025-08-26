15a0a45de906ad7931c1bb41371f9674dac69a8f
aldomedina <aldo.medina.z@gmail.com>
2025-08-26 16:49:17 -0400

    sobremenu link ahora redirecciona

--- src/components/Navbar/SobreMenu.jsx
+++ src/components/Navbar/SobreMenu.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
 import classNames from "classnames";
+
 import { LogoPHacker25 } from "../Logos";
+import { paths } from "../Router/routes";
+
 import css from "./Navbar.module.css";
 
 const SobreMenu = () => {
   return (
     <div className={css.sobremenu}>
-      <button className={css.logoPhacker25}>
+      <Link to={paths.home} className={css.logoPhacker25}>
         <LogoPHacker25 />
-      </button>
+      </Link>
+
       <div className={css.lang}>
         <button className={classNames("text-eyebrow", css.langButton)}>
           ESP
