9978e09c38a7c3f984d611ce3385e0d73d0bf7e2
aldomedina <aldo.medina.z@gmail.com>
2025-08-31 20:59:23 -0400

    bugfix: lang-menu in mobile

--- src/components/Navbar/MobileMenuDrawer.jsx
+++ src/components/Navbar/MobileMenuDrawer.jsx
@@ -1,12 +1,16 @@
 import cn from "classnames";
 import { Link, useNavigate } from "react-router-dom";
 import { paths } from "../Router/routes";
+import { LogoPrimaveraHacker } from "../Logos";
+import { useLang } from "../../store/lang";
 
 import css from "./Navbar.module.css";
-import { LogoPrimaveraHacker } from "../Logos";
 
 const MobileMenuDrawer = ({ isOpen, closeDrawer }) => {
   const navigate = useNavigate();
+  const { setEsp, setEng, lang, messages } = useLang();
+  const { evento, convocatoria, manifiesto, codigo } = messages();
+
   if (!isOpen) return null;
   return (
     <div className={css.drawer}>
@@ -24,35 +28,45 @@ const MobileMenuDrawer = ({ isOpen, closeDrawer }) => {
           to={paths.evento}
           className={cn("text-link-primary", css.primaryLink)}
         >
-          evento
+          {evento}
         </Link>
         <Link
           onClick={closeDrawer}
           to={paths.convocatoria}
           className={cn("text-link-primary", css.primaryLink)}
         >
-          convocatoria
+          {convocatoria}
         </Link>
         <Link
           onClick={closeDrawer}
           to={paths.manifiesto}
           className={cn("text-link-primary", css.primaryLink)}
         >
-          manifiesto
+          {manifiesto}
         </Link>
         <Link
           onClick={closeDrawer}
           to={paths.conducta}
           className={cn("text-link-primary", css.primaryLink)}
         >
-          codigo de conducta
+          {codigo}
         </Link>
       </div>
       <div className={css.lang}>
-        <button className={cn("text-link-primary", css.primaryLink)}>
-          ESP
+        <button
+          data-active={lang == "esp"}
+          className={cn("text-link-primary", css.primaryLink)}
+          onClick={setEsp}
+        >
+          {lang == "esp" && "→"} ESP
         </button>
-        <button className={cn("text-link-primary", css.primaryLink)}>EN</button>
+        <button
+          data-active={lang == "eng"}
+          className={cn("text-link-primary", css.primaryLink)}
+          onClick={setEng}
+        >
+          {lang == "eng" && "→"} ENG
+        </button>
       </div>
     </div>
   );
